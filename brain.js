31dcca189fdabbfcf071d40628113b84ea3a4a23
SubhamPanda2003 <97492779+SubhamPanda2003@users.noreply.github.com>
2022-01-25 11:16:59 +0530

    Update brain.js

--- brain.js
+++ brain.js
@@ -1,4 +1,5 @@
 let i=00;
+let textToSave="";
 function submit(){
     i++;
     var x = document.getElementById("myTextarea").value;
@@ -6,5 +7,20 @@ function submit(){
     console.log(i);
     let nav=document.getElementById(i);
     nav.innerHTML="<ul><li>"+x+"</li></ul>";
+    
+    textToSave = textToSave+" "+i+">>"+x;
+    
     document.getElementById("myTextarea").value="";
+
+}
+function download(){
+    let btn = document.getElementById("btn1");
+
+
+var hiddenElement = document.createElement('a');
+
+hiddenElement.href = 'data:attachment/text,' + encodeURI(textToSave);
+hiddenElement.target = '_blank';
+hiddenElement.download = 'myFile.txt';
+hiddenElement.click();
 }
